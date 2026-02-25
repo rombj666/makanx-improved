2f256b41ad8ac80b67bb8f5eac842c024f20d38d
Biao Jun <rombj45@gmail.com>
2026-02-25 19:06:46 +0800

    final

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@makanx/shared': path.resolve(__dirname, '../../packages/shared/dist')
-    }
-  }
+  plugins: [react()]
 })
