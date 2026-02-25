56f8925115042dff0ac92d22c8aa703e6952139e
Biao Jun <rombj45@gmail.com>
2026-02-25 19:41:16 +0800

    f

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,18 +1,9 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
-import { SocketProvider } from "./context/SocketContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <SocketProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </SocketProvider>
-    </AuthProvider>
+    <App />
   </StrictMode>
 );
